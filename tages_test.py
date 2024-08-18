b065053fe2fe45bd07ef1cfa01404b5cc3e34a17
zevisvei <101482799+zevisvei@users.noreply.github.com>
2024-08-18 07:44:07 +0300

    Update tages_test.py

--- tages_test.py
+++ tages_test.py
@@ -4,7 +4,7 @@ import shutil
 with open("not_seported.txt", "r", encoding = "utf-8") as ff:
 	list_tags = ff.read().splitlines()
 	for i in list_tags:
-		with open(f"{i}.txt", "r", encoding = "utf-8") as f:
+		with open(f"tag path//{i}.txt", "r", encoding = "utf-8") as f:
 			list_file = f.read().splitlines()
 			for path in list_file:
 				src = os.path.join("html", path)
