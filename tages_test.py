911b58060fa03f91e0fb7b6a40b1fa1db6d09b88
zevisvei <101482799+zevisvei@users.noreply.github.com>
2024-08-18 07:43:16 +0300

    Create tages_test.py

---
+++ tages_test.py
@@ -0,0 +1,14 @@
+import os
+import shutil 
+
+with open("not_seported.txt", "r", encoding = "utf-8") as ff:
+	list_tags = ff.read().splitlines()
+	for i in list_tags:
+		with open(f"{i}.txt", "r", encoding = "utf-8") as f:
+			list_file = f.read().splitlines()
+			for path in list_file:
+				src = os.path.join("html", path)
+				dest = os.path.join(i, path)
+				folder = os.path.join(os.path.split(dest)[:-1]) 
+				os.mkdir(folder, exist_ok=True)
+				shutil.copy(src, dest)
